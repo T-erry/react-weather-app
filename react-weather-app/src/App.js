9a9ccbb0f4d53d3b651ea6bbf711ab96f7b4b83e
Terry Wachuka <79338912+T-erry@users.noreply.github.com>
2022-08-09 21:02:51 +0300

    installed bootstrap

---
+++ react-weather-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
