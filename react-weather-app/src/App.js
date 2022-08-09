0cdda86d74c0ce86ef963f45b234bc0f65c29f8d
Terry Wachuka <79338912+T-erry@users.noreply.github.com>
2022-08-09 22:26:03 +0300

    created a form

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Hello</h1>
+        <Weather />
+        <footer>
+          This code was coded by{" "}
+          <a href="https://github.com/T-erry">Terry Wachuka</a>
+          and{" "}
+          <a
+            href="https://github.com/T-erry/react-weather-app.git"
+            target="-blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
---
+++ react-weather-app/src/Weather.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Weather.css";
+
+function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-sm-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-sm-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <div className="location">
+        <h1>Tanzania</h1>
+      </div>
+      <div className="time">
+        <ul>
+          <li>Tuesday 14:00</li>
+          <li>Mostly cloudy</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="mostly cloudy"
+          ></img>
+          <span className="temperature">10 °C</span>
+        </div>
+        <div className="col-sm-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 46%</li>
+            <li>Wind: 24 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Weather;
