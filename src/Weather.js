46030d37ca406808df4a5fe1209c4d3a802b8c52
Terry Wachuka <79338912+T-erry@users.noreply.github.com>
2022-08-15 12:33:43 +0300

    added weather icon animations

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       humidity: response.data.main.humidity,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
     });
   }
   function search() {
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherIcons from "./WeatherIcons";
 
 function WeatherInfo(props) {
   return (
@@ -18,8 +19,7 @@ function WeatherInfo(props) {
       </div>
       <div className="row">
         <div className="col-sm-6">
-          <img src={props.data.iconUrl} alt={props.data.description} />
-
+          <WeatherIcons code={props.data.icon} />
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-sm-6">
